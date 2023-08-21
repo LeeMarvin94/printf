7bd98429542b134ff0957d0c9101e18c3b185b16
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-08-21 15:22:01 +0100

    some changes

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+/* I include these libraries here for them to cover the global project scope */
+#include<stdarg.h>
+#include<stdio.h>
+#include<stdarg.h>
+int _printf(const char *format, ...);
+
+
+
+
+
+#endif
